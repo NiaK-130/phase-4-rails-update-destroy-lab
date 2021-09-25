d6a67f6cac20e2110a5b9689b4b7fe88e6d78cd0
Nia K <nsjkappen@gmail.com>
2021-09-25 16:54:13 -0700

    Completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,22 @@
 class PlantsController < ApplicationController
 
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end 
+  end 
+
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
+
   # GET /plants
   def index
     plants = Plant.all
